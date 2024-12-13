2df6209bf61e3316746f01e310e0495ac9e4bf45
Hina457 <princess4177812@gmail.com>
2024-12-13 05:26:36 -0800

    created build and update code

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@
 import Hero from "@/Components/Hero/Hero"
 import Feature from "@/Components/featureproduct/featureproduct"
 import Ourproduct from "@/Components/ourproduct/our"
-import Explore from "@/Components/Explore/Explore"
+
 import Shop from "./shop/page"
 import Pages from "./pages/page"
+import Explore from "@/Components/Explore/Explore"
 export default function Main(){
   return(
     <div>
