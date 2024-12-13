53fe8395b6e60512a167b4785a4f8207ee70f169
Hina457 <princess4177812@gmail.com>
2024-12-13 05:58:52 -0800

    remove explore component

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import Feature from "../Components/featureproduct/featureproduct"
 import Ourproduct from "../Components/ourproduct/our"
 import Shop from "./shop/page"
 import Pages from "./pages/page"
-import Explore from "../Components/Explore/Explore"
+// import Explore from "../Components/Explore/Explore"
 import Contact from "./Contact/page"
 export default function Main(){
   return(
     <div>
     <Hero/>
     <Feature/>
-    <Explore/>
+    {/* <Explore/> */}
     <Ourproduct/>
     <Shop/>
     <Pages/>
