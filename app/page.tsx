089116f21d4ce5e372a906b1c2e96225b4ba52e9
Hina457 <princess4177812@gmail.com>
2024-12-15 11:01:09 -0800

    Rename Components to components

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,12 @@ import Feature from "../components/featureproduct/featureproduct"
 import Ourproduct from "../components/ourproduct/our"
 import Shop from "./shop/page"
 import Pages from "./pages/page"
-// import Explore from "../Components/Explore/Explore"
 import Contact from "./Contact/page"
 export default function Main(){
   return(
     <div>
     <Hero/>
     <Feature/>
-    {/* <Explore/> */}
     <Ourproduct/>
     <Shop/>
     <Pages/>
